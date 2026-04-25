2c26cefece5d51d4212867d733e2915264e9e2a4
ycaiovinicius <caiooincrivel@gmail.com>
2026-04-25 00:04:13 -0300

    Acesso remoto

---
+++ CalcularMedia.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+int nota1, nota2, nota3;
+float media;
+
+    //Entrada de Dados
+        printf("***CALCULE SUA MÈDIA AQUI***\n");
+        printf("Insira sua primeira nota:\n");
+            scanf("%d", &nota1);
+
+                printf("Insira sua segunda nota:\n");
+                    scanf("%d", &nota2);
+
+                        printf("Insira sua terceira nota:\n");
+                            scanf("%d", &nota3);
+
+            //Calculando a média
+            media = (float) (nota1 + nota2 + nota3) / 3;
+
+    //Exibindo Resultado da média
+    printf("Sua média é: %.1f\n", media);
+
+        if (media >= 7) {
+            printf("Parabéns, você passou\n");
+        } else {
+            printf("Infelizmente, você não passou");
+        }
+
+    return 0;
+}
